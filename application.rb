32cc0e0380a80538ca16ca6d4fa92ac7de228e6e
Armando Couto <coutoarmando@gmail.com>
2022-09-17 19:46:42 -0300

    Testes

--- application.rb
+++ application.rb
@@ -1,2 +1,35 @@
+require 'csv'
+require 'json'
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'mysql2', '~> 0.5.4'
+end
 
 puts "Começando a Importar!"
+
+data = CSV.read("BookBuddy.csv")
+
+data.each do |item|
+  puts item
+end
+
+begin
+  client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "root", :database => "biblioteca")
+
+  client.query("SELECT * FROM books;").each do |row|
+    puts row
+  end
+
+  client.query("SELECT * FROM indexes;").each do |row|
+    puts row
+  end
+
+rescue Mysql2::Error => e
+  puts e.errno
+  puts e.error
+
+ensure
+  client.close if client
+end
